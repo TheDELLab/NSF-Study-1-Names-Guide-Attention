155a91b00013d95536bb7d7be55159fc30d90d24
r0han99 <r409067@gmail.com>
2023-05-31 15:57:20 -0600

    added all tthe revised changes post-pilot-study-one

--- Psychopy/Experiment.py
+++ Psychopy/Experiment.py
@@ -44,7 +44,7 @@ def animation_screen(win, orientation,imgs):
         image.pos = [x_pos, y_pos]
 
         # Loop through the spline points and move the image stimulus
-        for t in np.linspace(x.min(), x.max(), num=700):
+        for t in np.linspace(x.min(), x.max(), num=600):
             x_pos = t * win.size[0] - 1100  # adjust x_pos calculation
             y_pos = spline(t) * 900 - 500
             image.pos = [x_pos, y_pos]
@@ -83,7 +83,7 @@ def animation_screen(win, orientation,imgs):
         image.pos = [x_pos, y_pos]
 
         # Loop through the spline points and move the image stimulus
-        for t in np.linspace(x.min(), x.max(), num=700):
+        for t in np.linspace(x.min(), x.max(), num=600):
             x_pos = 960 - (t * win.size[0] - 200)  # adjust x_pos calculation
             y_pos = spline(t) * 900 - 500
             image.pos = [x_pos, y_pos]
@@ -204,6 +204,27 @@ while True:
 results_folder = 'results'
 if not os.path.exists(results_folder):
     os.makedirs(results_folder)
+    
+# Prompt user to specify the condition number
+dlg_title = 'Enter Condition Number'
+dlg_prompt = 'Please enter the condition number (1 or 2).'
+
+# Loop until we get a valid condition number
+while True:
+    dlg = gui.Dlg(dlg_title)
+    dlg.addText(dlg_prompt)
+    dlg.addField('Condition Number:', choices=['1', '2'])
+    # Show dialog and wait for OK or Cancel
+    ok_data = dlg.show()
+    if dlg.OK:  # If ok_data is not None
+        CONDITION_NUM = int(ok_data[0])
+        print('Condition Number: {}'.format(CONDITION_NUM))
+        break
+    else:
+        print('User cancelled')
+        core.quit()
+        sys.exit()
+
 
 # We download EDF data file from the EyeLink Host PC to the local hard
 # drive at the end of each testing session, here we rename the EDF to
@@ -228,6 +249,8 @@ imgs_r = cycle(imgs_r)
 
 # Create a window to display the stimuli
 win = visual.Window(fullscr=True, units='pix', color=(1, 1, 1))
+# info screen
+info = visual.TextStim(win, text='Press Space to Start Trial', pos=(0,-400), height=25, color='black')
 
 
 # Create a list to store the data for each trial
@@ -235,7 +258,7 @@ trials_data = []
 
 # Read in the conditions from the CSV file
 conditions = pd.read_csv("./conditions/Sample_Conditions.csv").drop('Unnamed: 0',axis=1)
-CONDITION_NUM = 2 ## CHANGE THE CONDITION NUMBER TO TOGGLE BETWEEN THE TRIAL TYPE ( Current options 1 or 2 ) 
+#CONDITION_NUM = 2 ## CHANGE THE CONDITION NUMBER TO TOGGLE BETWEEN THE TRIAL TYPE ( Current options 1 or 2 ) 
 cond = conditions[conditions['Conditions']==CONDITION_NUM] 
 cond = cond.reset_index(drop=True)
 cond = cond.sample(frac=1) # sample rows and with replacement ( Shuffles all the samples ) 
@@ -245,123 +268,235 @@ audio = cond['Audio']
 checkpoint_flag = 0
 iterator = 1
 
-for trial, audio in zip(trial_range, audio):
+
+def simulate_trial(win):
     
-    
-    # Create the stimuli
-    doll = visual.ImageStim(win, image='./images/Cookie-Monster-smaller.png',pos=(0,300))
-    number = visual.TextStim(win, text=cond.loc[trial, 'Target'], color='black', height=300, pos=(-10, 0) )
-
-    
-    # Audio Instantiation
-    audio = sound.Sound(f"./audio/{audio}.wav") 
-   
-   
-
-    left_number = visual.TextStim(win, text=cond.loc[trial, 'Target'], color='black', height=250, pos=(-400, 0))
-    right_number = visual.TextStim(win, text=cond.loc[trial, 'Foil'], color='black', height=250, pos=(400, 0))
-
-    # Display the doll
-    doll.draw()
-    win.flip()
-
-    # Wait for 3 seconds
-    time.sleep(3)
-
-    # Display the target number
-    number.draw()
+    info = visual.TextStim(win, text='Press Space to Simulate a Trial or Escape to Start the Actual Experiment', pos=(0,0), height=25, color='black')
+    note = visual.TextStim(win, text='Which one did you see first?', pos=(0,-400), height=25, color='black')
+    info.draw()
     win.flip()
         
-    # Play audio stimulus
-    audio.play()
-    
-    # Wait for audio to finish playing
-    core.wait(audio.getDuration())
-
-    # Wait for 3 seconds
-    time.sleep(3)
-
-    # Animate the plane
-    orientation = choice(orientation_list)
-    if orientation == 'normal':
-        animation_screen(win, orientation, imgs_n)
-    elif orientation == 'reverse':
-        animation_screen(win, orientation, imgs_r )
-
-
-
-    # start_time = time.time()
-    # while time.time() - start_time < 3:
-    #     plane.setPos([plane.pos[0] + 0.01, plane.pos[1] + 0.01])
-    #     if plane.pos[0] > 1:
-    #         plane.setPos([1, 1])
-    #     plane.draw()
-    #     win.flip()
-
-    # Display the foils
-    left_number.draw()
-    right_number.draw()
-    win.flip()
-
-    # Wait for a key press
-    start_time = time.time()
-    keys = event.waitKeys(keyList=['left', 'right'])
-    response_time = round((time.time()-start_time), 3)
-    response_key = keys[0]
-
-
-    # --- animation sequence from here, doesn't have any prevalence on the response time recording.
-
-    # Animate the selected number
-    if response_key == 'left':
-        selected_number = left_number
-    else:
-        selected_number = right_number
-
-    start_pos = selected_number.pos
-    end_pos = doll.pos
-
-    start_time = time.time()
-    while time.time() - start_time < 0.5:
-        pos = [start_pos[0] + (end_pos[0] - start_pos[0]) * (time.time() - start_time) / 0.5,
-                start_pos[1] + (end_pos[1] - start_pos[1]) * (time.time() - start_time) / 0.5]
-        selected_number.setPos(pos)
-
-        # Draw the doll and the selected number
-        doll.draw()
-        selected_number.draw()
-
-        # Flip the screen to update the display
-        win.flip()
-
-    # Record the trial data
-
-    
-    trial_data = {
-    
-    'trial_id': trial,
-    'condition': CONDITION_NUM,
-    'num_presentation_type': cond.loc[trial, 'Number_type'],
-    'audio_presentation_context': cond.loc[trial, 'Audio'],
-    'target_number': cond.loc[trial, 'Target'],
-                    'foil': cond.loc[trial, 'Foil'],
-                    'response_key': response_key,
-                    'response_time': response_time
-                    }
-    trials_data.append(trial_data)
-    
-    if  iterator % 6 == 0: # 4 Check points and 24 Trials, so 24/4 every Sixth Trial is a check point
-        checkpoint_flag += 1 
-        checkpoint = visual.ImageStim(win, image=f'./images/checkpoint{checkpoint_flag}.png', pos=(0,0))
-        # Display the checkpoint image
-        checkpoint.draw()
-        win.flip()
         
-        # Wait for 3 seconds
+    control_key = event.waitKeys(keyList=['space','escape'])
+
+    if control_key[0] == 'space':
+    
+
+        target_image = visual.ImageStim(win, image='./images/turtle.png', pos=(-10, 0))
+        target_left_image = visual.ImageStim(win, image='./images/turtle.png', pos=(-400, 0))
+        foil_left_image = visual.ImageStim(win, image='./images/elephant.png', pos=(400, 0))
+
+        target_image.draw()
+        win.flip()
+
         time.sleep(3)
+
+        target_left_image.draw()
+        foil_left_image.draw()
+        note.draw()
+        win.flip()
+
+        time.sleep(3)
+
+        # Wait for a key press
+        start_time = time.time()
+        keys = event.waitKeys(keyList=['left', 'right', 'escape'])
+        response_time = round((time.time() - start_time), 3)
+        response_key = keys[0]
+
+        if response_key == 'left':
+            # Animate the left image to the center
+            duration = 2.5  # Animation duration in seconds
+            frames_per_second = 60  # Number of frames per second
+            num_frames = int(duration * frames_per_second)
+
+            # Calculate the position change per frame
+            pos_change = np.array((410, 0)) / num_frames
+
+            # Animate the image
+            for frame in range(num_frames):
+                target_left_image.pos += pos_change
+                target_left_image.draw()
+                foil_left_image.draw()
+                win.flip()
+            
     
-    iterator += 1  
+    elif control_key[0] == 'escape':
+        
+        return None
+
+
+
+
+simulate_trial(win)
+
+path = visual.ImageStim(win, image='./images/path.png',pos=(0,0))
+path.draw()
+info.draw()
+win.flip()
+
+control_key = event.waitKeys(keyList=['space'])
+
+if control_key[0] == 'space':
     
+    for trial, audio in zip(trial_range, audio):
+        
+    
+
+        # Create the stimuli
+        doll = visual.ImageStim(win, image='./images/Cookie-Monster-smaller.png',pos=(0,300))
+        number = visual.TextStim(win, text=cond.loc[trial, 'Target'], color='black', height=300, pos=(-10, 0) )
+        
+    
+        
+        # Audio Instantiation
+        audio = sound.Sound(f"./audio/{audio}.wav") 
+       
+        # Target position 
+        target_location = cond.loc[trial,'Location']
+        if cond.loc[trial,'Location'] == 'left':
+            left_number = visual.TextStim(win, text=cond.loc[trial, 'Target'], color='black', height=250, pos=(-400, 0))
+            right_number = visual.TextStim(win, text=cond.loc[trial, 'Foil'], color='black', height=250, pos=(400, 0))
+        else:
+            left_number = visual.TextStim(win, text=cond.loc[trial, 'Foil'], color='black', height=250, pos=(-400, 0))
+            right_number = visual.TextStim(win, text=cond.loc[trial, 'Target'], color='black', height=250, pos=(400, 0))
+
+       
+        # Display the doll
+        doll.draw()
+        info.draw()
+        win.flip()
+
+        # Wait for 3 seconds
+        #time.sleep(3)
+        
+        control_key = event.waitKeys(keyList=['space'])
+
+        if control_key[0] == 'space':
+            
+            #time.sleep(0.5) # Just to be ready
+
+            # Display the target number
+            number.draw()
+            win.flip()
+                
+            # Play audio stimulus
+            audio.play()
+            
+            # Wait for audio to finish playing
+            core.wait(audio.getDuration())
+
+            # Wait for 4 seconds
+            time.sleep(4)
+
+            # Animate the plane
+            orientation = choice(orientation_list)
+            if orientation == 'normal':
+                animation_screen(win, orientation, imgs_n)
+            elif orientation == 'reverse':
+                animation_screen(win, orientation, imgs_r )
+
+
+            # Display the number and foil
+            left_number.draw()
+            right_number.draw()
+            win.flip()
+       
+                
+                
+                
+            # Wait for a key press
+            start_time = time.time()
+            keys = event.waitKeys(keyList=['left', 'right','escape'])
+            response_time = round((time.time()-start_time), 3)
+            response_key = keys[0]
+            
+            
+            # ACCURACY
+            if response_key == target_location:
+                accuracy = 1
+            else:
+                accuracy = 0
+
+
+            # --- animation sequence from here, doesn't have any prevalence on the response time recording.
+
+            # Animate the selected number
+            if response_key == 'left':
+                selected_number = left_number
+            elif response_key == 'right':
+                selected_number = right_number
+            elif response_key == 'escape':
+                print('aborting')
+                
+                
+                
+                trial_data = {
+                
+                'trial_id': trial,
+                'condition': CONDITION_NUM,
+                'num_presentation_type': cond.loc[trial, 'Number_type'],
+                'audio_presentation_context': cond.loc[trial, 'Audio'],
+                'other_features': cond.loc[trial, "Other_features"],
+                'target_number': cond.loc[trial, 'Target'],
+                'foil': cond.loc[trial, 'Foil'],
+                'response_key': response_key,
+                'response_time': response_time,
+                'accuracy': accuracy,
+                                }
+                trials_data.append(trial_data)
+                
+                break
+                
+
+            start_pos = selected_number.pos
+            end_pos = doll.pos
+
+            start_time = time.time()
+            while time.time() - start_time < 1.5:
+                pos = [start_pos[0] + (end_pos[0] - start_pos[0]) * (time.time() - start_time) / 1.5,
+                        start_pos[1] + (end_pos[1] - start_pos[1]) * (time.time() - start_time) / 1.5]
+                selected_number.setPos(pos)
+
+                # Draw the doll and the selected number
+                doll.draw()
+                selected_number.draw()
+
+                # Flip the screen to update the display
+                win.flip()
+
+            # Record the trial data
+
+            
+            trial_data = {
+            
+            'trial_id': trial,
+            'condition': CONDITION_NUM,
+            'num_presentation_type': cond.loc[trial, 'Number_type'],
+            'audio_presentation_context': cond.loc[trial, 'Audio'],
+            'other_features': cond.loc[trial, "Other_features"],
+            'target_number': cond.loc[trial, 'Target'],
+            'accuracy': accuracy,
+            'foil': cond.loc[trial, 'Foil'],
+            'response_key': response_key,
+            'response_time': response_time
+                            }
+            trials_data.append(trial_data)
+            
+            if  iterator % 6 == 0: # 4 Check points and 24 Trials, so 24/4 every Sixth Trial is a check point
+                checkpoint_flag += 1 
+                checkpoint = visual.ImageStim(win, image=f'./images/checkpoint{checkpoint_flag}.png', pos=(0,0))
+                # Display the checkpoint image
+                checkpoint.draw()
+                win.flip()
+                
+                # Wait for 3 seconds
+                time.sleep(3)
+            
+            iterator += 1  
+           
+        else:
+            pass
 
 end_message = visual.TextStim(win, text="Saving Data ...", color='black', height=20, pos=(0, 0))
 end_message.draw()
@@ -370,7 +505,7 @@ time.sleep(2)
 
 # Write the data to a CSV file
 with open(f'{session_folder}/{file_name}_data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['trial_id', 'condition','num_presentation_type','audio_presentation_context','target_number', 'foil', 'response_key', 'response_time']
+    fieldnames = ['trial_id', 'condition','num_presentation_type','audio_presentation_context','other_features','target_number','accuracy','foil', 'response_key', 'response_time']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for trial_data in trials_data:
